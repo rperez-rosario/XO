41a1a9c0228f53de3b570042a05acec25744c1b6
Rafael Pérez <rperezrosario@outlook.com>
2021-05-29 16:21:25 -0400

    Initial XOSkinWebApp seed files (C-Sharp).

---
+++ XOSkinWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XOSkinWebApp.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
