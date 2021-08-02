cc8c459f7302f379d2f52b68210b2ad66ad97ec1
Rafael Pérez <rperezrosario@outlook.com>
2021-08-02 18:06:35 -0400

    Additional skeleton page work.

--- XOSkinWebApp/Controllers/YourLipsAndSmileController.cs
+++ XOSkinWebApp/Controllers/YourLipsAndSmileController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace XOSkinWebApp.Controllers
 {
-  public class LipsAndSmileController : Controller
+  public class YourLipsAndSmileController : Controller
   {
     public IActionResult Index()
     {
